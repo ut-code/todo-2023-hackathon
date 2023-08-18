0728b6c6dab31bebcdbf8ea1385444ca620a9d12
Masafiro <141376324+Masafiro@users.noreply.github.com>
2023-08-18 10:49:13 +0900

    Taskクラスのプロパティ名をnameからtitleに修正、response.sendを追加

--- main.js
+++ main.js
@@ -7,26 +7,30 @@ app.use(express.urlencoded({ extended: true }));
 
 const tasks = [];
 class Task {
-    name;
+    title;
     deadline;
     importance;
-    constructor(name, deadline, importance) {
-        this.name = name;
+    constructor(title, deadline, importance) {
+        this.title = title;
         this.deadline = deadline;
         this.importance = importance;
     };
 };
 
 app.get("/", (request, response) => {
-    response.send("hello");
+    const template = fs.readFileSync("index.ejs", "utf8");
+    const html = ejs.render(template, {tasks: tasks});
+    response.send(html);
 });
 
 app.post("/edit", (request, response) => {
+    response.send(編集画面);
 });
 
 app.post("/", (request, response) => {
-    const task = new Task(request.body.name, request.body.deadline, request.body.importance,);
+    const task = new Task(request.body.title, request.body.deadline, request.body.importance,);
     tasks.push(task);
     const template = fs.readFileSync("index.ejs", "utf8");
     const html = ejs.render(template, {tasks: tasks});
+    response.send(html);
 });
