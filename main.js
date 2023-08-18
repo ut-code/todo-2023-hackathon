9127cf78e841b9490841fa407f178ece42872654
Masafiro <141376324+Masafiro@users.noreply.github.com>
2023-08-18 10:21:41 +0900

    バックエンドのファイルを追加

---
+++ main.js
@@ -0,0 +1,32 @@
+const fs = require("fs");
+const ejs = require("ejs");
+const express = require("express");
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+
+const tasks = []
+class Task {
+    name;
+    deadline;
+    importance;
+    constructor(name, deadline, importance) {
+        this.name = name;
+        this.deadline = deadline;
+        this.importance = importance
+    }
+}
+
+app.get("/", (request, response) => {
+    response.send("hello")
+});
+
+app.post("/edit", () => {
+});
+
+app.post("/", (request, response) => {
+    const task = new Task(request.body.name, request.body.deadline, request.body.importance,)
+    tasks.push(task)
+    const template = fs.readFileSync("index.ejs", "utf8");
+    const html = ejs.render(template, {tasks: tasks});
+});
