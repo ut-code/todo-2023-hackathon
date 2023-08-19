ed8a89e0d0bea513f82b2f7ad3d823a80e2fd973
kazukinumazato <101048462+kazukinumazato@users.noreply.github.com>
2023-08-19 14:09:24 +0900

    バックエンド接続

--- main.js
+++ main.js
@@ -5,36 +5,45 @@ const app = express();
 const { PrismaClient } = require("@prisma/client");
 const client = new PrismaClient();
 
-const userId = 777
+const userId = 1;
 
 async function main(request, response) {
-    const tasks = await client.task.findMany({ userId });
-    const template = fs.readFileSync("index.ejs", "utf8");
-    const html = ejs.render(template, { tasks: tasks });
-    response.send(html);
-  }
+  const tasks = await client.task.findMany({ where: { userId } });
+  const template = fs.readFileSync("output.ejs", "utf8");
+  const html = ejs.render(template, { tasks: tasks });
+  response.send(html);
+}
 
 async function push(request, response, userId, name, due, isImportant) {
-    await client.task.create({ data: {  userId, name, due, isImportant } });
-    const tasks = await client.task.findMany({ userId });
-    const template = fs.readFileSync("index.ejs", "utf8");
-    const html = ejs.render(template, { tasks: tasks });
-    response.send(html);
-  }
+  await client.task.create({ data: { userId, name, due, isImportant } });
+  const tasks = await client.task.findMany({ where: { userId } });
+  const template = fs.readFileSync("output.ejs", "utf8");
+  const html = ejs.render(template, { tasks: tasks });
+  response.send(html);
+}
 
 app.use(express.urlencoded({ extended: true }));
 
+app.use(express.static("static"));
+
 app.get("/", (request, response) => {
-    main(request, response);
+  main(request, response);
 });
 
 app.post("/edit", (request, response) => {
-    const html = fs.readFileSync("input.html", "utf8");
-    response.send(html);
+  const html = fs.readFileSync("input.html", "utf8");
+  response.send(html);
 });
 
 app.post("/", (request, response) => {
-    push(request, response, userId, request.body.name, request.body.due, request.body.isImportant);
+  push(
+    request,
+    response,
+    userId,
+    request.body.name,
+    new Date(request.body.due),
+    request.body.isImportant === "on" ? true : false
+  );
 });
 
 app.listen(3000);
